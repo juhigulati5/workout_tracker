50b920dce0934c7c310212f8cbf314be5df37d8f
juhig <juhigulati5@gmail.com>
2024-04-03 11:12:39 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+import os
+from datetime import datetime
+
+APP_ID = os.environ["APP_ID"]
+API_KEY = os.environ["API_KEY"]
+
+today = datetime.now()
+TIME = today.strftime("%X")
+DATE = today.strftime("%m/%d/%Y")
+
+USERNAME = os.environ["USERNAME"]
+PASSWORD = os.environ["PASSWORD"]
+
+
+NUTRIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEETY_ENDPOINT = os.environ["SHEET_ENDPOINT"]
+
+QUERY = input("Tell me which exercises you did: ")
+
+headers = {
+    'x-app-id': APP_ID,
+    'x-app-key': API_KEY,
+    "Content-Type": "application/json"
+}
+
+parameters = {
+    "query": QUERY,
+    "weight_kg": 90,
+    "height_cm": 162.56,
+    "age": 22
+}
+response = requests.post(url=NUTRIONIX_ENDPOINT, json=parameters, headers=headers)
+response.raise_for_status()
+data = response.json()
+exercise_data = data["exercises"][0]
+
+for exercise in data["exercises"]:
+    user_params = {
+        "workout": {
+            "date": DATE,
+            "time": TIME,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(
+        url=SHEETY_ENDPOINT,
+        json=user_params,
+        auth=(USERNAME, PASSWORD)
+    )
+
+    print(sheet_response.text)
+
